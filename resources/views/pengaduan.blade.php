b58bb02063ada49f00b8e1310337119c0583db69
naochizuki <basugandhisasangka@student.uns.ac.id>
2022-12-15 16:33:10 +0700

    Update

--- resources/views/pengaduan.blade.php
+++ resources/views/pengaduan.blade.php
@@ -1,12 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Pengaduan</title>
-</head>
-<body>
-    <p>Halaman Pengaduan</p>
-</body>
-</html>
+@extends('layout.master')
+
+@section('content')
+    <form action="/pengaduan" method="POST" class="form-pengaduan-container">
+        @csrf
+        <div class="pengaduan-container">
+            <div class="pengaduan-1st-container">
+                <div class="input-container">
+                    <label for="nama" class="text-orange unbounded label">Nama</label>
+                    <input type="text" name="nama" id="nama" class="border-bottom">
+                </div>
+                <div class="input-container">
+                    <label for="gender_umur" class="text-orange unbounded label">Jenis Kelamin & Umur</label>
+                    <input type="text" name="gender_umur" id="gender_umur" class="border-bottom">
+                </div>
+                <div class="input-container">
+                    <label for="alamat" class="text-orange unbounded label">Alamat</label>
+                    <input type="text" name="alamat" id="alamat" class="border-bottom">
+                </div>
+                <div class="input-container">
+                    <label for="email" class="text-orange unbounded label">Email</label>
+                    <input type="email" name="email" id="email" class="border-bottom">
+                </div>
+            </div>
+            <div class="pengaduan-2nd-container">
+                <div class="input-container">
+                    <label for="deskripsi" class="text-orange unbounded label">Deskripsi</label>
+                    <textarea name="deskripsi" id="deskripsi" cols="30" rows="10"></textarea>
+                </div>
+                <div class="submit-container">
+                    <button type="submit" class="button-submit text-white unbounded">Kirim</button>
+                </div>
+            </div>
+        </div>
+    </form>
+@endsection
